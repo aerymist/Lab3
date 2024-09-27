ec0be6ad2edb9e916b409fd7083cdb80fcde6887
aerymist <leiliahxy@gmail.com>
2024-09-26 23:34:09 -0400

    fixed bug on main

--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -59,7 +59,7 @@ public class Main {
             //            convert it back to its 2-letter language code when calling translate.
             //            Note: you should use the actual names in the message printed below though,
             //            since the user will see the displayed message.
-            System.out.println(country + " in " + language + " is " + translator.translate(country, lcc.languageToCode(language)));
+            System.out.println(country + " in " + language + " is " + translator.translate(country, lcc.fromLanguage(language)));
             System.out.println("Press enter to continue or quit to exit.");
             Scanner s = new Scanner(System.in);
             String textTyped = s.nextLine();
